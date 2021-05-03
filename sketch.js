ff13fca94549dff1fafa0a67c07d6b7ebe9a0f1f
Priyanka Berlia <priyanka.parasrampuria@gmail.com>
2021-05-03 11:30:58 +0530

    pl removed

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
     
-    pl= new Ground(600,height,1200,20)
+    //pl= new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -62,5 +62,5 @@ function draw(){
 
     bird.display();
 
-    pl.display();
+    //pl.display();
 }
